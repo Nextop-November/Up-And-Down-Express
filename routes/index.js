e34a9e8b0db0ef8e3383bd58fd8e01a8b00c52c0
민상연 <judemin2087@naver.com>
2020-02-24 15:52:22 +0900

    Crawler_test

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { getConnection } = require('typeorm');
 const Laptop = require('../schemas/laptopSchem');
+var Crawler = require("crawler");
 
 router.get ('/', function(req,res,next) {
   const connection = getConnection();
@@ -9,6 +10,29 @@ router.get ('/', function(req,res,next) {
   repository.find().then((result) => {
     res.status(200).json(result);
   });
+});
+
+router.get ('/crawler', function(req,res,next) {
+  var c = new Crawler({
+    maxConnections : 10,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+
+            console.log($("title").text());
+            console.log($('p[class=prod_name]'));
+            console.log($('a[class=prod_name]'));
+            console.log($("html").find("a[class='prod_name']").text());
+        }
+        done();
+    }
+  });
+
+  c.queue('http://prod.danawa.com/list/?cate=112758');
+
+  res.status(200).json("Test Crawler");
 });
 
 module.exports = router;
