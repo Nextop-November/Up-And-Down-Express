883f098f05972c6418f2e34a7c840b7a01ac86d2
민상연 <judemin2087@naver.com>
2020-02-25 11:36:13 +0900

    Puppeteer

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { getConnection } = require('typeorm');
 const Laptop = require('../schemas/laptopSchem');
-var Crawler = require("crawler");
+const puppeteer = require('puppeteer');
 
 router.get ('/', function(req,res,next) {
   const connection = getConnection();
@@ -13,26 +13,42 @@ router.get ('/', function(req,res,next) {
 });
 
 router.get ('/crawler', function(req,res,next) {
-  var c = new Crawler({
-    maxConnections : 10,
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
+  getProductHref('http://prod.danawa.com/list/?cate=112758');
 
-            console.log($("title").text());
-            console.log($('p[class=prod_name]'));
-            console.log($('a[class=prod_name]'));
-            console.log($("html").find("a[class='prod_name']").text());
-        }
-        done();
-    }
-  });
-
-  c.queue('http://prod.danawa.com/list/?cate=112758');
-
-  res.status(200).json("Test Crawler");
+  res.status(200).json("Test Carwler");
 });
+
+function getProductHref(url){
+  let elements = [];
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    
+    await page.waitForSelector("a[class='prod_name']", {timeout: 10000});
+    const result = await page.evaluate(() => {
+        const anchors = Array.from(document.querySelectorAll("a[class='prod_name']"));
+        return anchors.map(anchor => anchor.getAttribute('href'));
+    });
+    elements.push(result);
+    //console.log(result.join('\n'));
+
+    await page.waitForSelector("a[name='productName']", {timeout: 10000});
+    const result_ = await page.evaluate(() => {
+        const anchors = Array.from(document.querySelectorAll("a[name='productName']"));
+        return anchors.map(anchor => anchor.getAttribute('href'));
+    });
+    elements.push(result_);
+    //console.log(result_.join('\n'));
+    await browser.close();
+
+    console.log("Crawling done at " + url);
+    console.log(elements);
+    })();
+
+    return elements;
+}
 
 module.exports = router;
