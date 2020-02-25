ab4e0f486bbabec298fc9281118eed37e6b3a303
민상연 <judemin2087@naver.com>
2020-02-25 21:53:13 +0900

    각 제품 크롤링

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const { getConnection } = require('typeorm');
 const Laptop = require('../schemas/laptopSchem');
 const puppeteer = require('puppeteer');
 
+let productElements = [];
+
 router.get ('/', function(req,res,next) {
   const connection = getConnection();
   const repository = connection.getRepository(Laptop.options.name);
@@ -18,37 +20,53 @@ router.get ('/crawler', function(req,res,next) {
   res.status(200).json("Test Carwler");
 });
 
-function getProductHref(url){
-  let elements = [];
+async function getProductHref(url){
 
-  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    await page.waitForSelector("a[name='productName']", {timeout: 10000});
+    const result = await page.evaluate(() => {
+        const anchors = Array.from(document.querySelectorAll("a[name='productName']"));
+        return anchors.map(anchor => anchor.getAttribute('href'));
+    });
+    productElements.push(...result);
+    //console.log(result_.join('\n'));
+    await browser.close();
+
+    console.log("Crawling done at " + url);
+    console.log(productElements);
+
+    console.log("Queue crawling start");
+    var i;
+    for(i = 0;i < productElements.length;i++){
+        await getSingleProductInfo(productElements[i]);
+    }
+    console.log("Queue crawling finished");
+}
+
+async function getSingleProductInfo(url){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto(url);
     
-    await page.waitForSelector("a[class='prod_name']", {timeout: 10000});
+    await page.waitForSelector(".lowest_price a.lwst_prc", {timeout: 10000});
     const result = await page.evaluate(() => {
-        const anchors = Array.from(document.querySelectorAll("a[class='prod_name']"));
-        return anchors.map(anchor => anchor.getAttribute('href'));
-    });
-    elements.push(result);
-    //console.log(result.join('\n'));
+        const anchors = Array.from(document.querySelectorAll(".lowest_price a.lwst_prc"));
+        return anchors.map(anchor => {
+          const price =  anchor.textContent;
+          const url = anchor.getAttribute('href');
 
-    await page.waitForSelector("a[name='productName']", {timeout: 10000});
-    const result_ = await page.evaluate(() => {
-        const anchors = Array.from(document.querySelectorAll("a[name='productName']"));
-        return anchors.map(anchor => anchor.getAttribute('href'));
+          return {price,url};
+        });;
     });
-    elements.push(result_);
-    //console.log(result_.join('\n'));
-    await browser.close();
-
     console.log("Crawling done at " + url);
-    console.log(elements);
-    })();
+    console.log(result);
 
-    return elements;
+    await browser.close();
 }
 
 module.exports = router;
