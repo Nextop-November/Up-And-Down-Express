ffa288740fd2d5f46e9a5b7445ead7618d9722bd
민상연 <judemin2087@naver.com>
2020-03-17 21:38:44 +0900

    가격변동 추적

--- routes/laptop.js
+++ routes/laptop.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { getConnection } = require('typeorm');
 const Laptop = require('../schemas/laptopSchem');
+const PriceTransition = require('../models/priceTransition').PriceTransition;
 
 router.get('/', function(req, res, next) {
     const connection = getConnection();
@@ -20,5 +21,43 @@ router.post('/', function(req, res, next) {
     });
     res.status(201).json();
 });
+
+router.get('/price', function(req, res, next) {
+    const lapId = parseInt(req.query.id);
+    getPriceTrans(lapId);
+
+    res.status(200).json("LaptopID : " + lapId);
+});
+
+async function getPriceTrans(id){
+    let priceTrans = [];
+
+    timestamp = new Date();
+    timestampLim = new Date();
+    timestampLim.setMonth(timestampLim.getMonth() - 3);
+
+    let laptopInfo = await getConnection()
+    .getRepository(PriceTransition)
+    .createQueryBuilder("PriceTransition")
+    .where("PriceTransition.laptopId = :tmp and PriceTransition.date between :tmp1 and :tmp2 "
+        , { tmp: id, tmp1: timestampLim, tmp2 : timestamp})
+    .select("PriceTransition")
+    .orderBy("PriceTransition.date")
+    .getMany();
+
+    for(i = 0;i < laptopInfo.length;i++){
+        let tmp = [];
+        tmp.push({date :laptopInfo[i].date,
+            price :laptopInfo[i].price,
+            websiteAddress :laptopInfo[i].websiteAddress,
+            cash :laptopInfo[i].cash,
+            websiteAddressCash :laptopInfo[i].websiteAddressCash
+        });
+
+        priceTrans.push(...tmp);
+    }
+
+    console.log(priceTrans);
+}
 
 module.exports = router;
